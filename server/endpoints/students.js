1fa16fdd9b22b93f4631152e81c6c419008299c2
aperdomob <aperdomobo@gmail.com>
2016-09-10 13:52:45 -0500

    remove some technical debt in stundents file

--- server/endpoints/students.js
+++ server/endpoints/students.js
@@ -1,28 +1,32 @@
-var express = require('express');
-var firebase = require("firebase");
-var router = express.Router();
+const express = require('express');
+const firebase = require('firebase');
+
+const router = express.Router();
 const database = firebase.database();
 const studentsRef = database.ref('/students');
 
-router.post('/', (request, response) =>{
-  var createdStudent = studentsRef.push(request.body);
+router.post('/', (request, response) => {
+  const createdStudent = studentsRef.push(request.body);
   response.send(createdStudent.key);
 });
-router.get('/', (request, response)=>{
-  studentsRef.on('value', (students) =>{
+
+router.get('/', (request, response) => {
+  studentsRef.on('value', (students) => {
     response.send(students.val());
   });
 });
-router.get('/:id', (request, response)=>{
-  let studentRef = database.ref(`/students/${request.params.id}`);
+
+router.get('/:id', (request, response) => {
+  const studentRef = database.ref(`/students/${request.params.id}`);
   studentRef.on('value', (student) => {
     response.send(student.val());
   });
 });
-router.delete('/:id', (request, response) =>{
+
+router.delete('/:id', (request, response) => {
   // TODO: REVISAR, CAN'T SET HEADERS AFTER THEY ARE SEN'T
-  let studentRef = database.ref(`/students/${request.params.id}`);
-  var operation = studentRef.remove().then(() => {
+  const studentRef = database.ref(`/students/${request.params.id}`);
+  const operation = studentRef.remove().then(() => {
     response.send();
   });
 });
